fde4680079e8eae0da334f0396fd0128d923631f
klausuy <83480269+klausuy@users.noreply.github.com>
2022-06-18 21:33:02 -0300

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,48 +1,44 @@
 ## CLUSTER
 
-resource "aws_eks_cluster" "eks-cluster-obl" {
-  name     = "example"
-  role_arn = aws_iam_role.example.arn
+resource "aws_eks_cluster" "cluster_obl" {
+  name     = "cluster_obl"
+  role_arn = aws_iam_role.cluster_obl_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+    subnet_ids = [aws_subnet.private-1a.id]
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.cluster_obl_role-AmazonEKSClusterPolicy,
   ]
 }
 
 output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
+  value = aws_eks_cluster.cluster_obl.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
+  value = aws_eks_cluster.cluster_obl.certificate_authority[0].data
 }
 
 ## WORKER
 
-resource "aws_eks_node_group" "eks-work-obl" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "example"
-  node_role_arn   = aws_iam_role.example.arn
-  subnet_ids      = aws_subnet.example[*].id
+resource "aws_eks_node_group" "worker_obl" {
+  cluster_name    = aws_eks_cluster.cluster_obl.name
+  node_group_name = "worker_obl"
+  node_role_arn   = aws_iam_role.worker_obl_role.arn
+  subnet_ids      = [aws_subnet.private-1a.id]
+  instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 2
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.worker_obl_role-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.worker_obl_role-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.worker_obl_role-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
